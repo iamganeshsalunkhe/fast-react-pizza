4e56d73acc8a1303d9a4000263fdf22c9099c198
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2024-12-17 15:08:35 +0530

    loader is added in menu route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from './ui/Home';
-import Menu from './features/menu/Menu';
+import Menu, {loader as menuLoader }from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
 import AppLayout from './ui/AppLayout';
 
 
-const router = createBrowserRouter([
+const router = createBrowserRouter([ 
   {
     element: <AppLayout />,
     children: [
@@ -18,6 +18,7 @@ const router = createBrowserRouter([
       {
         path: "menu",
         element: <Menu />,
+        loader:menuLoader
       },
       {
         path: "/cart",
