aa30c9c7924185c99825141f322296418ef08734
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2024-12-30 11:22:12 +0530

    new font add

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username"
 
 function Header() {
     return (
-        <header className= "flex items-center justify-between bg-yellow-400 uppercase px-4 py-3 border-b border-stone-400 ">
+        <header className= "flex items-center justify-between bg-yellow-400 uppercase px-4 py-3 border-b border-stone-400 font-sans">
             <Link to ='/' className="tracking-widest" >Fast React Pizza Co. </Link>
 
         <SearchOrder/>
